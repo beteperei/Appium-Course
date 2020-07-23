4090a6e03f59607e14d00d2978c545c56bee3cd0
unknown <bertha.basico@gmail.com>
2020-07-22 23:41:09 -0500

    first push

---
+++ src/test/java/Basics.java
@@ -0,0 +1,23 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class Basics extends Base{
+    public static void main(String[] args) throws MalformedURLException{
+      AndroidDriver<AndroidElement> driver = Capabilities();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //xpath syntax tagName[@attribute='value']
+
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+        driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
+        driver.findElementsByClassName("android.widget.Button").get(1).click();
+
+
+    }
+
+}
---
+++ src/test/java/UiautomatorTest.java
@@ -0,0 +1,20 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class UiautomatorTest extends Base {
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.findElementByAndroidUIAutomator("(attribute('value')");
+
+        driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+        driver.findElementByAndroidUIAutomator("text(\"Animation\")").click();
+
+        //Validate clickable feature for all options
+        System.out.println(driver.findElementsByAndroidUIAutomator("new UiSelector().clickable(true)").size());
+
+    }
+}
