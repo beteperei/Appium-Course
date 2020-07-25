eded00a818a6788aa6cc300517b0014ac582e247
unknown <bertha.basico@gmail.com>
2020-07-25 00:01:24 -0500

    Lesson 35

--- src/test/java/Gestures.java
+++ src/test/java/Gestures.java
@@ -1,9 +1,15 @@
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.LongPressOptions;
+import io.appium.java_client.touch.TapOptions;
+import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.offset.ElementOption.element;
 
 public class Gestures extends Base{
     public static void main(String[] args) throws MalformedURLException {
@@ -11,10 +17,21 @@ public class Gestures extends Base{
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
-        driver.findElementByXPath("\\android.widget.TextView[@text='Expandable Lists']").click();
 
         //Tap
         TouchAction t = new TouchAction(driver);
+
+        WebElement expandList = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
+        t.tap(TapOptions.tapOptions().withElement(element(expandList))).perform();
+
+        driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']").click();
+
+        WebElement peopleNames = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+        t.longPress(LongPressOptions.longPressOptions().withElement(element(peopleNames)).withDuration(Duration.ofSeconds(2))).release().perform();
+
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
+
+
 
     }
 }
---
+++ src/test/java/SwipeDemo.java
@@ -0,0 +1,20 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class SwipeDemo extends Base {
+
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Base.Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
+        driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
+        driver.findElementByXPath("//*[@content-desc='9']").click();
+
+    }
+}
