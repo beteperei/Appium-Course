4b26c559bc427b260a9646b20c9182e52e18cdad
unknown <bertha.basico@gmail.com>
2020-09-09 20:54:18 -0500

    Test Case 03

--- src/test/java/GeneralStore_TestCase02.java
+++ src/test/java/GeneralStore_TestCase02.java
@@ -1,11 +1,13 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.junit.Test;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 public class GeneralStore_TestCase02 extends Base_generalstore {
+    @Test
     public static void main(String[] args) throws MalformedURLException {
         AndroidDriver<AndroidElement> driver = Capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
---
+++ src/test/java/GeneralStore_TestCase03.java
@@ -0,0 +1,36 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class GeneralStore_TestCase03 extends Base_generalstore {
+    @Test
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Bertha");
+        driver.hideKeyboard();
+        driver.findElementById("com.androidsample.generalstore:id/radioFemale").click();
+        driver.findElementById("com.androidsample.generalstore:id/spinnerCountry").click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+        driver.findElementByXPath("//android.widget.TextView[@text='Argentina']").click();
+        driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
+
+        driver.findElementsByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))");
+        int count = driver.findElementsById("com.androidsample.generalstore:id/productName").size();
+
+        for (int i =0;i <count; i++){
+          String  text = driver.findElementsById("com.androidsample.generalstore:id/productName").get(i).getText();
+            if (text.equalsIgnoreCase("Jordan 6 Rings")){
+                driver.findElementsById("com.androidsample.generalstore:id/productAddCart").get(i).click();
+                break;
+            }
+        }
+
+        driver.findElementById("com.androidsample.generalstore:id/appbar_btn_cart").click();
+    }
+
+}
